4eb07f67514e426ab292b87a86b0eda2e4611fa6
Alan Perez <alanp9six@gmail.com>
2019-06-18 14:57:27 -0500

    fixed the size of the facebook icon

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,17 +5,25 @@ import SEO from '@/seo'
 import GoogleLoginButton from 'src/images/google-login-button.png'
 import FacebookLoginButton from 'src/images/facebook-login-button.png'
 
+const fbStyle = {
+  width: '260px',
+  height: '43px'
+}
+const contStyle = {
+  margin: '0 auto',
+  width: '50%'
+}
 const LoginPage = () => (
   <Layout>
     <SEO title="Login" />
 
-    <Container>
+    <Container style={contStyle}>
       <a href={`${process.env.GATSBY_API_URL}/auth/google`}>
         <img src={GoogleLoginButton} alt="" />
       </a>
 
       <a href={`${process.env.GATSBY_API_URL}/auth/facebook`}>
-        <img src={FacebookLoginButton} alt="" />
+        <img src={FacebookLoginButton} style={fbStyle} alt="" />
       </a>
     </Container>
   </Layout>
